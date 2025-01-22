28421d4f0ff5840c8e9511d5c8ca608f0f2adf06
Fernando Sena <fernando.sena@mkdata.com.br>
2025-01-22 11:55:44 -0300

    feat: init project. adds dependencies and configs

---
+++ src/main/java/br/com/compass/HibernateUtil.java
@@ -0,0 +1,27 @@
+package br.com.compass;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            return new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Falha na criação da SessionFactory: " + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+}
+
